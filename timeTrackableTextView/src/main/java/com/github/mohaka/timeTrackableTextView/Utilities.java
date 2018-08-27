15f6a8acb900bdee76bc73206bf81d701c5e17f2
Haytham KATBY <mohakapt@gmail.com>
2018-08-27 12:46:43 +0300

    - Initial commit

---
+++ timeTrackableTextView/src/main/java/com/github/mohaka/timeTrackableTextView/Utilities.java
@@ -0,0 +1,16 @@
+package com.github.mohaka.timeTrackableTextView;
+
+import android.content.Context;
+
+/**
+ * Created by MoHaKa on 2/7/17.
+ */
+class Utilities {
+    private Context context;
+
+    static Utilities with(Context context) {
+        Utilities reVal = new Utilities();
+        reVal.context = context;
+        return reVal;
+    }
+}
